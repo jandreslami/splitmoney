e0c00ae66e95b8e2807f91134142fa090d9c4ed3
jandreslami <115507664+jandreslami@users.noreply.github.com>
2023-11-08 09:41:58 -0300

    Create modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { SettleModule } from './settle/settle.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    UserModule,
+    TransactionsModule,
+    PaymentsModule,
+    SettleModule,
+  ],
 })
 export class AppModule {}
