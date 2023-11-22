22a17654cea25a62d85bb720b56051b25ec5b595
jandreslami <115507664+jandreslami@users.noreply.github.com>
2023-11-22 11:05:27 -0300

    Create simple AuthDto logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signUp() {
+  signUp(@Body() dto: AuthDto) {
+    console.log(dto);
     return this.authService.signUp();
   }
 
   @Post('login')
-  logIn() {
+  logIn(@Body() dto: AuthDto) {
     return this.authService.logIn();
   }
 }
